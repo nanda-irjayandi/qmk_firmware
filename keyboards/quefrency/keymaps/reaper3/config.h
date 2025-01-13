6ec92c10acdca6880598055ce61becc477412196
Nanda Irjayandi <nanda.irjayandi@gmail.com>
2025-01-14 00:42:57 +1100

    refactor: rgb blinking during macro recording

--- keyboards/quefrency/keymaps/reaper3/config.h
+++ keyboards/quefrency/keymaps/reaper3/config.h
@@ -19,10 +19,6 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-#pragma once
-
-// #define USE_I2C
-
 //User Defined Variables
 #define ONESHOT_TIMEOUT 3000
 #define BOTH_SHIFTS_TURNS_ON_CAPS_WORD
--- keyboards/quefrency/keymaps/reaper3/keymap.c
+++ keyboards/quefrency/keymaps/reaper3/keymap.c
@@ -9,6 +9,8 @@
 /* bool     is_alt_tab_active = false;
 uint16_t alt_tab_timer     = 0; */
 
+bool is_recording = false;
+
 enum custom_keycodes {
     QWERTY = SAFE_RANGE,
 //    RGB_SPUP,
@@ -17,13 +19,13 @@ enum custom_keycodes {
     ALT_GRV, */
 };
 
-const rgblight_segment_t PROGMEM DEV_LAYER[]  = RGBLIGHT_LAYER_SEGMENTS({0, 16, HSV_GREEN});
-const rgblight_segment_t PROGMEM DATA_LAYER[] = RGBLIGHT_LAYER_SEGMENTS({0, 16, HSV_BLUE});
-const rgblight_segment_t PROGMEM MOD_LAYER[]  = RGBLIGHT_LAYER_SEGMENTS({0, 16, HSV_RED});
-const rgblight_segment_t PROGMEM FUNCTION_LAYER[]  = RGBLIGHT_LAYER_SEGMENTS({8, 1, HSV_RED});
-const rgblight_segment_t PROGMEM MENU_LAYER[] = RGBLIGHT_LAYER_SEGMENTS({8, 1, HSV_WHITE});
+const rgblight_segment_t PROGMEM DEV_LAYER[]          = RGBLIGHT_LAYER_SEGMENTS({0, 16, HSV_GREEN});
+const rgblight_segment_t PROGMEM DATA_LAYER[]         = RGBLIGHT_LAYER_SEGMENTS({0, 16, HSV_BLUE});
+const rgblight_segment_t PROGMEM MOD_LAYER[]          = RGBLIGHT_LAYER_SEGMENTS({0, 16, HSV_RED});
+const rgblight_segment_t PROGMEM FUNCTION_LAYER[]     = RGBLIGHT_LAYER_SEGMENTS({8, 1, HSV_RED});
+const rgblight_segment_t PROGMEM MENU_LAYER[]         = RGBLIGHT_LAYER_SEGMENTS({8, 1, HSV_WHITE});
 
-/* the lowest overrides other lightning layer */
+/* the right-most or bottom-most from above lime blocks overrides other lightning layer */
 const rgblight_segment_t* const PROGMEM _rgb_layers[] = RGBLIGHT_LAYERS_LIST(DEV_LAYER, DATA_LAYER, MOD_LAYER, FUNCTION_LAYER, MENU_LAYER);
 
 /* initialize rgb_layers */
@@ -34,7 +36,10 @@ void keyboard_post_init_user(void) {
 layer_state_t layer_state_set_user(layer_state_t state) {
     rgblight_set_layer_state(0, layer_state_cmp(state, 4));  // Dev Layer
     rgblight_set_layer_state(1, layer_state_cmp(state, 6));  // Data Layer
-    rgblight_set_layer_state(3, layer_state_cmp(state, 7) || layer_state_cmp(state, 8) || layer_state_cmp(state, 9));  // Data Layer
+    rgblight_set_layer_state(3,
+        layer_state_cmp(state, 7) ||
+        layer_state_cmp(state, 8) ||
+        layer_state_cmp(state, 9));  // Data Layer
     rgblight_set_layer_state(4, layer_state_cmp(state, 10)); // Menu Layer
     return state;
 }
@@ -49,11 +54,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
 
   [1] = LAYOUT_65_with_macro( /* Win Linux BSD Layer */
-    LCTL(KC_C), LCTL(KC_V),     QK_GESC, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6,                                      KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL, KC_BSPC, KC_BSPC,                                               KC_HOME,
-    LCTL(KC_Z), LCTL(KC_Y),     KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T,                                             KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC, KC_BSLS,                                                 KC_PGUP,
-    MEH(KC_F5), MEH(KC_F6),     LT(7, KC_BSPC), LGUI_T(KC_A), LALT_T(KC_S), LCTL_T(KC_D), LSFT_T(KC_F), KC_G,     KC_H, RSFT_T(KC_J), RCTL_T(KC_K), RALT_T(KC_L), RGUI_T(KC_SCLN), KC_QUOT, LT(6, KC_ENT),                 KC_PGDN,
-    MEH(KC_F7), MEH(KC_F8),     OSM(MOD_LSFT), KC_Z, KC_X, KC_C, KC_V, KC_B,                                      KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT, KC_UP,                                                    KC_END,
-    DM_PLY1, DM_PLY2,           OSM(MOD_LCTL), OSM(MOD_LGUI), OSM(MOD_LALT), OSL(8), LT(9, KC_SPC),               KC_SPC, KC_SPC, OSM(MOD_RALT), OSM(MOD_RGUI), OSM(MOD_RCTL), KC_LEFT, LT(10, KC_DOWN),                   KC_RGHT
+    LCTL(KC_C), LCTL(KC_V),     QK_GESC, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6,                                      KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL, KC_BSPC, KC_BSPC,                                   KC_HOME,
+    LCTL(KC_Z), LCTL(KC_Y),     KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T,                                             KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC, KC_BSLS,                                     KC_PGUP,
+    MEH(KC_F5), MEH(KC_F6),     LT(7, KC_BSPC), LGUI_T(KC_A), LALT_T(KC_S), LCTL_T(KC_D), LSFT_T(KC_F), KC_G,     KC_H, RSFT_T(KC_J), RCTL_T(KC_K), RALT_T(KC_L), RGUI_T(KC_SCLN), KC_QUOT, LT(6, KC_ENT),     KC_PGDN,
+    MEH(KC_F7), MEH(KC_F8),     OSM(MOD_LSFT), KC_Z, KC_X, KC_C, KC_V, KC_B,                                      KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT, KC_UP,                                        KC_END,
+    DM_PLY1, DM_PLY2,           OSM(MOD_LCTL), OSM(MOD_LGUI), OSM(MOD_LALT), OSL(8), LT(9, KC_SPC),               KC_SPC, KC_SPC, OSM(MOD_RALT), OSM(MOD_RGUI), OSM(MOD_RCTL), KC_LEFT, LT(10, KC_DOWN),       KC_RGHT
   ),
 
   [4] = LAYOUT_65_with_macro( /* Speedtyping Layer or when you are annoyed by the lag from the home row modifier TODO separate game speedype layer with dev layer the latter use space cadet */
@@ -83,7 +88,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [7] = LAYOUT_65_with_macro( /* Function 1 Layer QUICK and SPECIAL Access ACS Caps Lock WIP */
     MEH(KC_KP_1), MEH(KC_KP_2),      KC_GRV, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6,                    KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, KC_DEL, KC_DEL,                                         MEH(KC_HOME),
     MEH(KC_KP_3), MEH(KC_KP_4),      MEH(KC_TAB),  MEH(KC_Q), KC_UP, MEH(KC_E), MEH(KC_R), MEH(KC_T),     MEH(KC_Y), MEH(KC_U), MEH(KC_I), MEH(KC_O), MEH(KC_P), MEH(KC_LBRC), MEH(KC_RBRC), MEH(KC_BSLS),     MEH(KC_PGUP),
-    MEH(KC_KP_5), MEH(KC_KP_6),      KC_NO, KC_LEFT, KC_DOWN, KC_RGHT, MEH(KC_F), MEH(KC_G),              KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, MEH(KC_SCLN), MEH(KC_QUOT), MEH(KC_ENT),                           MEH(KC_PGDN),
+    MEH(KC_KP_5), MEH(KC_KP_6),      KC_NO, KC_LEFT, KC_DOWN, KC_RGHT, MEH(KC_F), MEH(KC_G),              KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, KC_TRNS, MEH(KC_QUOT), MEH(KC_ENT),                           MEH(KC_PGDN),
     MEH(KC_KP_7), MEH(KC_KP_8),      KC_TRNS, MEH(KC_Z), MEH(KC_X), MEH(KC_C), MEH(KC_V), MEH(KC_B),      MEH(KC_N), MEH(KC_M), MEH(KC_COMM), MEH(KC_DOT),  MEH(KC_SLSH), KC_TRNS, MEH(KC_UP),                 MEH(KC_END),
     MEH(KC_KP_9), MEH(KC_KP_0),      KC_TRNS, KC_TRNS, KC_TRNS, TG(8), MEH(KC_SPC),                       MEH(KC_SPC), MEH(KC_SPC), KC_TRNS, KC_TRNS, KC_TRNS, MEH(KC_LEFT), MEH(KC_DOWN),                     MEH(KC_RGHT)
   ),
@@ -105,11 +110,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
 
   [10] = LAYOUT_65_with_macro( /* Menu Layer NOTE the Right Mod is there for Shiroyama Encoder RGB */
-    RGB_MOD, RGB_RMOD,      TG(5), TG(6), TG(4), KC_NO, KC_NO, KC_NO, KC_NO,        KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, QK_RBT,     RGB_SPI,
-    KC_NO, KC_NO,           KC_NO, KC_VOLD, KC_MUTE, KC_VOLU, KC_NO, KC_NO,         KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,     RGB_VAI,
-    OS_OFF, OS_ON,          KC_CAPS, KC_MPRV, KC_MPLY, KC_MNXT, QK_LOCK, KC_NO,     KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, TG(6),            RGB_VAD,
-    NK_OFF, NK_ON,          KC_NO, KC_BRID, KC_F15, KC_BRIU, KC_NO, KC_NO,           KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,            RGB_SPD,
-    DM_REC1, DM_REC2,       KC_NO, DF(1), DF(0), TG(8), TG(10),                     KC_NO, KC_NO, KC_RGUI, KC_RALT, KC_RCTL, RGB_HUD, KC_NO,    RGB_HUI
+    RGB_MOD, RGB_RMOD,     TG(5), TG(6), TG(4), KC_NO, KC_NO, KC_NO, KC_NO,        KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, QK_RBT,    RGB_SPI,
+    KC_NO, KC_NO,          KC_NO, KC_VOLD, KC_MUTE, KC_VOLU, KC_NO, KC_NO,         KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,     RGB_VAI,
+    OS_OFF, OS_ON,         KC_CAPS, KC_MPRV, KC_MPLY, KC_MNXT, KC_NO, KC_NO,     KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, TG(6),            RGB_VAD,
+    NK_OFF, NK_ON,         KC_NO, KC_BRID, KC_F15, KC_BRIU, KC_NO, KC_NO,          KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,            RGB_SPD,
+    DM_REC1, DM_REC2,      KC_NO, DF(1), DF(0), TG(8), TG(10),                     KC_NO, KC_NO, KC_RGUI, KC_RALT, KC_RCTL, RGB_HUD, KC_NO,    RGB_HUI
   )
 };
 
@@ -119,24 +124,76 @@ void oneshot_mods_changed_user(uint8_t mods) {
 
 void caps_word_set_user(bool active) {
         rgblight_set_layer_state(2, is_caps_word_on());
-}
-
-void dynamic_macro_record_start_user(int8_t direction) {
-    rgblight_blink_layer_repeat(2, 1000, 150); // blink for five minute
-    rgblight_disable_noeeprom();
 };
 
-void dynamic_macro_record_end_user(int8_t direction) {
-    rgblight_unblink_layer(2);
-    rgblight_enable_noeeprom();
-};
+// 20250113 seems to be deprecated
+// void dynamic_macro_record_start_user(int8_t direction) {
+//     rgblight_blink_layer_repeat(2, 1000, 150); // blink for five minute
+//     rgblight_disable_noeeprom();
+// };
+
+// void dynamic_macro_record_end_user(int8_t direction) {
+//     rgblight_unblink_layer(2);
+//     rgblight_enable_noeeprom();
+// };
+
+// bool dynamic_macro_record_start_kb(int8_t direction) {
+//     if (!dynamic_macro_record_start_user(direction)) {
+//         return false;
+//     }
+//     rgblight_blink_layer_repeat(2, 1000, 150); // blink for five minute
+//     rgblight_disable_noeeprom(); // hide underlying layer while recording
+
+//     return true;
+// };
+
+// bool dynamic_macro_record_end_kb(int8_t direction) {
+//     if (!dynamic_macro_record_end_user(direction)) {
+//         return false;
+//     }
+//     rgblight_unblink_layer(2);
+//     rgblight_enable_noeeprom();
+
+//     return true;
+// };
+
+// const uint16_t PROGMEM test_combo1[] = {KC_C, KC_M, COMBO_END};
+
+// combo_t key_combos[] = {
+//     COMBO(test_combo1, KC_ESC),
+// };
 
 // Custom Keycodes
 // Disabled in rev 3 as no use for now see Rev 2 for history
 bool process_record_user(uint16_t keycode, keyrecord_t* record) {
-
     return true; // process all keycodes normally
 };
+
+void handle_recording_blink(bool is_recording, uint8_t layer, uint16_t duration_ms) {
+    if (is_recording) {
+        rgblight_blink_layer_repeat(layer, duration_ms, UINT8_MAX / 2); // Simulates indefinite blink
+    } else {
+        rgblight_unblink_layer(layer);
+    }
+}
+
+bool dynamic_macro_record_start_kb(int8_t direction) {
+    if (!dynamic_macro_record_start_user(direction)) {
+        return false;
+    }
+    handle_recording_blink(true, 2, 1000);
+    rgblight_disable_noeeprom();
+    return true;
+}
+
+bool dynamic_macro_record_end_kb(int8_t direction) {
+    if (!dynamic_macro_record_end_user(direction)) {
+        return false;
+    }
+    handle_recording_blink(false, 2, 1000);
+    rgblight_enable_noeeprom();
+    return true;
+}
 
 // note that matrix_scan_user is taxing for the processor and increases lag and I am disabling it
 /* void matrix_scan_user(void) {
